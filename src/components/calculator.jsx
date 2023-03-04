afb6daa009200c3224641a594b4abcea27dede87
Megan Carlan <mgcarlan@gmail.com>
2023-03-04 12:54:21 -0600

    decimals are enabled

--- src/components/calculator.jsx
+++ src/components/calculator.jsx
@@ -9,7 +9,6 @@ export function Calculator() {
     const [operator, setOperator] = useState("");
     const [previousKeyType, setPreviousKeyType] = useState("");
     const [firstValue, setFirstValue] = useState(0);
-    const [result, setResult] = useState(0);
 
 
 
@@ -52,13 +51,13 @@ export function Calculator() {
         let result;
 
         if(operator == "add") {
-            result = firstNumber + secondNumber;
+            result = parseFloat(firstNumber) + parseFloat(secondNumber);
         } else if(operator == "subtract") {
-            result = firstNumber - secondNumber;
+            result = parseFloat(firstNumber) - parseFloat(secondNumber);
         } else if (operator == "multiply") {
-            result = firstNumber * secondNumber;
+            result = parseFloat(firstNumber) * parseFloat(secondNumber);
         } else if(operator == "divide") {
-            result = firstNumber/secondNumber;
+            result = parseFloat(firstNumber)/parseFloat(secondNumber);
         }
         
         console.log(result);
@@ -156,31 +155,10 @@ console.log(firstValue);
                     setPreviousKeyType("equals");}
                     } className="key_equal">=</button>
             </div>
-
-
             
             
             
             
-            {/* <div className="calculator_keys">
-                <button className="key_operator" data-action="add">+</button>
-                <button className="key_operator" data-action="subtract">-</button>
-                <button className="key_operator" data-action="multiply">&times;</button>
-                <button className="key_operator" data-action="divide">÷</button>
-                <button>7</button>
-                <button>8</button>
-                <button>9</button>
-                <button>4</button>
-                <button>5</button>
-                <button>6</button>
-                <button>1</button>
-                <button>2</button>
-                <button>3</button>
-                <button>0</button>
-                <button data-action="decimal">.</button>
-                <button data-action="clear">AC</button>
-                <button className="key_equal" data-action="calculate">=</button> */}
-</div>
-        // </div>
+        </div>
     );
 }
